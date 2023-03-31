36a6ee05dba797f3f031214120b7e8109dc63894
azimovgleb <azimov.gleb96@gmail.com>
2023-03-31 15:06:32 +0300

    Init commit

---
+++ src/test/java/com/azimov96/springsecurityproject/SpringSecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azimov96.springsecurityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
